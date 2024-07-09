f4ea21bc2d70f2f1e99521f847fa1237cd3abdd2
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2024-07-09 06:29:58 +0000

    Project mark process

---
+++ setup_aws_cli.sh
@@ -0,0 +1,7 @@
+# bash script to install aws cli on linux
+pwd=$(pwd)
+cd /tmp
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+cd $pwd
